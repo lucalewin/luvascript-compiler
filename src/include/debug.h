64fe9011145f862a1d622e75ad68b07be09e88ea
lucalewin <luca.lewin23@gmail.com>
2021-03-17 21:19:25 +0100

    add prototype function

--- src/include/debug.h
+++ src/include/debug.h
@@ -9,6 +9,7 @@ void debugPrintTab(int amount);
 
 void debugPrintExpression(Expr *expr, int depth);
 void debugPrintStatement(Statement *st, int depth);
+void debugPrintFuncParam(FuncParam *func_param, int depth);
 void debugPrintFunction(Function *func, int depth);
 
 #endif // LUVA_DEBUG_H
