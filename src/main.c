22745ca8ebd593ce982af04b455cc9b05df732fa
lucalewin <luca.lewin23@gmail.com>
2021-04-03 13:46:27 +0200

    remove unused includes

--- src/main.c
+++ src/main.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//#include "include/util.h"
-// #include "include/arraylist.h"
-// #include "include/lexer.h"
-// #include "include/parser.h"
-// #include "include/compiler.h"
-// #include "include/ast.h"
 #include <util.h>
 #include <arraylist.h>
 #include <lexer.h>
@@ -21,9 +15,12 @@ int main(int argc, char **argv) {
         return -1;
     }
 
-    int last_index = str_last_index_of(argv[1], '.');
-    char *file_base_name = malloc(last_index);
-    substring(argv[1], file_base_name, last_index);
+    int optimize = 0;
+    int optimizaionLvl = 3;
+
+    int last_index = str_last_index_of(argv[1], '.');   //
+    char *file_base_name = malloc(last_index);          // get filepath without extension
+    substring(argv[1], file_base_name, last_index);     //
 
     
     char* file_contents = read_file(argv[1]);           // read file
@@ -33,16 +30,15 @@ int main(int argc, char **argv) {
     AST *ast = parser_create_ast(list);                 // create AST from tokens
     arraylist_free(list);                               // free tokenlist
 
-    // ---------------------------------
 
-    // parser_generate_assembly_from_expr(file_base_name, NULL);
+    if (optimize) {
+        // Todo implement optimization
+    }
 
-    // ---------------------------------
+
+    char *asm = parser_convert_ast_to_assembly(ast);
 
     // compile(file_base_name);
-
-    /* run generated assembly */
-    // system(file_base_name);
 
     free(file_base_name);
 
