dd49c4a59080655a4a6db4677df6e7d060d82232
lucalewin <luca.lewin23@gmail.com>
2021-04-03 17:50:30 +0200

    create simple function template

--- src/x86-64/parser.c
+++ src/x86-64/parser.c
@@ -1,12 +1,17 @@
 #include <parser.h>
 #include <x86-64/parser.h>
 
-char *parser_convert_ast_to_assembly(AST *ast) {
-
+char *convert_ast_to_x86_64_assembly(AST *ast) {
+    
+    return "";
 }
 
 char *parser_convert_function_to_assembly(Function *func) {
-
+    char *template = "global %s\n%s:\n";
+    char *s = calloc(sizeof(template) + 2 * sizeof(func->name), sizeof(char));
+    sprintf(s, template, func->name, func->name);
+    printf("%s\n", s);
+    return s;
 }
 
 char *parser_convert_statement_to_assembly(Statement *stmt) {
