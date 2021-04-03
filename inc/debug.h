ba638dea1b2657da7bc9c164611b04cb9e507f14
lucalewin <luca.lewin23@gmail.com>
2021-04-03 17:47:12 +0200

    add prototype for FuncReturnType debug print

--- inc/debug.h
+++ inc/debug.h
@@ -10,6 +10,7 @@ void debugPrintTab(int amount);
 void debugPrintExpression(Expr *expr, int depth);
 void debugPrintStatement(Statement *st, int depth);
 void debugPrintFuncParam(FuncParam *func_param, int depth);
+void debugPrintFuncReturnType(FuncReturnType *return_type, int depth);
 void debugPrintFunction(Function *func, int depth);
 
 void debugPrintType(DataType *datatype, int depth);
