c092b1954a8ef60da0bec4f67e79ea69525161b3
lucalewin <luca.lewin23@gmail.com>
2021-04-03 14:17:08 +0200

    remove unnessesary qoutes

--- tests/test.sh
+++ tests/test.sh
@@ -9,9 +9,9 @@ index=1
 
 for file in "tests"/*.lvs
 do
-    echo "";
-    echo "---------------- [Test "$index"] ----------------"
-    echo "";
+    echo ""
+    echo "---------------- [Test $index] ----------------"
+    echo ""
     ./bin/lvc $file
     index=$((index+1));
 done
