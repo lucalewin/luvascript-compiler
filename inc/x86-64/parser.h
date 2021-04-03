070137c43bde2219e71d3d276dd79462bac40484
lucalewin <luca.lewin23@gmail.com>
2021-04-03 17:49:03 +0200

    remove unused prototypes + includes

--- inc/x86-64/parser.h
+++ inc/x86-64/parser.h
@@ -3,14 +3,13 @@
 
 #include <util.h>
 #include <arraylist.h>
-#include <token.h>
 #include <type.h>
 #include <expression.h>
 #include <statement.h>
 #include <function.h>
 #include <ast.h>
+#include <x86-64/context.h>
 
-char *parser_convert_ast_to_assembly(AST *ast);
 char *parser_convert_function_to_assembly(Function *func);
 char *parser_convert_statement_to_assembly(Statement *stmt);
 char *parser_convert_expression_to_assembly(Expr* expr);
