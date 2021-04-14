3da83e29b203d404f9c936fd0ddd99bf571644c9
lucalewin <luca.lewin23@gmail.com>
2021-04-14 18:19:38 +0200

    make all vars to pointer

--- src/parser2.c
+++ src/parser2.c
@@ -7,12 +7,14 @@ Token *current;
 void parse(AST *ast, ArrayList *list) {
     tokens = list;
     while (index < tokens->size) {
-        Function func;
-        VariableDecl var_decl;
-        if (expect_function(&func)) {
-            arraylist_add(ast->functions, &func);
-        } else if (expect_var_decl(&var_decl)) {
-            arraylist_add(ast->global_var_decls, &var_decl);
+        Function *func = malloc(sizeof(Function));
+        VariableDecl *var_decl = malloc(sizeof(VariableDecl));
+        if (expect_function(func)) {
+            arraylist_add(ast->functions, func);
+            free(var_decl);
+        } else if (expect_var_decl(var_decl)) {
+            arraylist_add(ast->global_var_decls, var_decl);
+            free(func);
         } else {
             printf("ERROR #0001: Expected Function or Variable declaration!\n");
             exit(1);
@@ -46,9 +48,9 @@ int expect_function(Function *func) {
             printf("ERROR #0003: Expected '(' at [%d,%d]\n", current->line, current->pos);
             exit(1);
         }
-        FuncParam param;
-        if (expect_func_param_decl(&param)) {
-            arraylist_add(func->params, &param);
+        FuncParam *param = malloc(sizeof(FuncParam));
+        if (expect_func_param_decl(param)) {
+            arraylist_add(func->params, param);
         }
         current = arraylist_get(tokens, index++);
         if (!expect_token(TOKEN_RPAREN, current)) {
@@ -58,9 +60,9 @@ int expect_function(Function *func) {
         current = arraylist_get(tokens, index++);
         if (expect_token(TOKEN_COLON, current)) {
             current = arraylist_get(tokens, index++);
-            FuncReturnType func_ret_type;
-            if (expect_func_return_type(&func_ret_type)) {
-                arraylist_add(func->return_types, &func_ret_type);
+            FuncReturnType *func_ret_type = malloc(sizeof(FuncReturnType));
+            if (expect_func_return_type(func_ret_type)) {
+                arraylist_add(func->return_types, func_ret_type);
             } else {
                 printf ("ERROR #0007: Expected function return type delaration after ':' at [%d,%d]\n", current->line, current->pos);
                 exit(1);
@@ -75,9 +77,9 @@ int expect_function(Function *func) {
         current = arraylist_get(tokens, index++);
         while (!expect_token(TOKEN_RBRACE, current)) {
             index--;
-            Statement stmt;
-            if (expect_statement(&stmt)) {
-                arraylist_add(func->statements, &stmt);
+            Statement *stmt = malloc(sizeof(Statement));
+            if (expect_statement(stmt)) {
+                arraylist_add(func->statements, stmt);
             } else {
                 printf("ERROR #0006: Expected '}' at [%d,%d]\n", current->line, current->pos);
                 exit(1);
@@ -99,9 +101,9 @@ int expect_func_param_decl(FuncParam *func_param) {
 
 int expect_func_return_type(FuncReturnType *func_ret_type) {
     printf("expect FUNCTION_RETURN_TYPE\n");
-    DataType type;
-    if (expect_datatype(&type)) {
-        func_ret_type->type = &type;
+    DataType *type = malloc(sizeof(DataType));
+    if (expect_datatype(type)) {
+        func_ret_type->type = type;
         if (!expect_token(TOKEN_ASSIGNMENT_SIMPLE, arraylist_get(tokens, index+1))) {
             // next token is not '='
             // and no default value is defined
@@ -110,13 +112,13 @@ int expect_func_return_type(FuncReturnType *func_ret_type) {
         }
         // parse expression
         current = arraylist_get(tokens, index += 2);
-        Expr expr;
-        if (!expect_expression(&expr)) {
+        Expr *expr = malloc(sizeof(Expr));
+        if (!expect_expression(expr)) {
             // Todo: print line + position
             printf("ERROR #0008: Expected expression after '='!\n");
             exit(1);
         }
-        func_ret_type->default_value = &expr;
+        func_ret_type->default_value = expr;
         return 1;
     }
     return 0;
@@ -124,20 +126,20 @@ int expect_func_return_type(FuncReturnType *func_ret_type) {
 
 int expect_statement(Statement *stmt) {
     printf("expect STATEMENT\n");
-    BlockStatement b_stmt;
-    ReturnStatement r_stmt;
-    ExprStatement e_stmt;
-    if (expect_block_statement(&b_stmt)) {
+    BlockStatement *b_stmt = malloc(sizeof(BlockStatement));
+    ReturnStatement *r_stmt = malloc(sizeof(ReturnStatement));
+    ExprStatement *e_stmt = malloc(sizeof(ExprStatement));
+    if (expect_block_statement(b_stmt)) {
         stmt->type = STATEMENT_BLOCK;
-        stmt->statement = &b_stmt;
+        stmt->statement = b_stmt;
         return 1;
-    } else if (expect_return_statement(&r_stmt)) {
+    } else if (expect_return_statement(r_stmt)) {
         stmt->type = STATEMENT_RETURN;
-        stmt->statement = &r_stmt;
+        stmt->statement = r_stmt;
         return 1;
-    } else if (expect_expression_statement(&e_stmt)) {
+    } else if (expect_expression_statement(e_stmt)) {
         stmt->type = STATEMENT_EXPR;
-        stmt->statement = &e_stmt;
+        stmt->statement = e_stmt;
     } else {
         printf("ERROR #0009: Unexpected statement");
         return 0;
@@ -158,9 +160,9 @@ int expect_block_statement(BlockStatement *b_stmt) {
 int expect_return_statement(ReturnStatement *r_stmt) {
     if (expect_token(TOKEN_KEYWORD, current) && strcmp(current->data, "return") == 0) {
         current = arraylist_get(tokens, index++);
-        Expr expr;
-        if (expect_expression(&expr)) {
-            r_stmt->expr = &expr;
+        Expr *expr = malloc(sizeof(Expr));
+        if (expect_expression(expr)) {
+            r_stmt->expr = expr;
             return 1;
         }
         r_stmt->expr = NULL;
@@ -169,9 +171,9 @@ int expect_return_statement(ReturnStatement *r_stmt) {
 }
 
 int expect_expression_statement(ExprStatement *e_stmt) {
-    Expr expr;
-    if (expect_expression(&expr)) {
-        e_stmt->expr = &expr;
+    Expr *expr = malloc(sizeof(Expr));
+    if (expect_expression(expr)) {
+        e_stmt->expr = expr;
         return 1;
     }
     return 0;
