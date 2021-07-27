12e1405a4c3050528cd0fb02fab12b023edfa079
lucalewin <luca.lewin23@gmail.com>
2021-07-28 00:17:02 +0200

    add comment + new method prototype

--- inc/lexer.h
+++ inc/lexer.h
@@ -1,13 +1,24 @@
 #ifndef LUVA_LEXER_H
 #define LUVA_LEXER_H
 
+// ----------------------------------------------
+
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "arraylist.h"
-#include "util.h"
-#include "token.h"
+#include <arraylist.h>
+#include <util.h>
+#include <token.h>
+#include <assembly.h>
 
+// ----------------------------------------------
+
+// main method prototype to lex the code
 ArrayList *lexer_start(char *code);
+
+// prototype methods for lexing inline x86-64 assembly
+Assembly *lexer_parse_x86_64(char *code);
+
+// ----------------------------------------------
 
 #endif // LUVA_LEXER_H
