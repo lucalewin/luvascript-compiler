c4330323fb8cc9b0c0e1a94d64498f3cf5bcd2a4
lucalewin <luca.lewin23@gmail.com>
2021-07-27 15:06:33 +0200

    add basic type structures

--- inc/x86-64/asm/assembly.h
+++ inc/x86-64/asm/assembly.h
@@ -1,28 +1,62 @@
 #ifndef LUVA_x86_64_ASSEMBLY
 #define LUVA_x86_64_ASSEMBLY
 
-typedef struct _func Func;
-typedef struct _var Var;
+// -------------------------------
 
-typedef struct _block Block;
+typedef enum { data, rodata, bss, text } SectionType;
+typedef enum { db, dw, dd, dq, ddq, dt } VarType;
+typedef enum { } InstType;
 
-struct _assembly {
+// -------------------------------
+
+typedef struct {
+    // sections
+    Section *sections;
+    int sectionCount = 0;
+} Assembly;
+
+typedef struct {
+    // type of section
+    SectionType type;
     // array of functions
     Func *functions;
+    int funcCount = 0;
+    // array of constants
+    Const *constants;
+    int constCount = 0;
     // array of variables
     Var *vars;
-};
+    int varCount = 0;
+} Section;
 
-struct _func {
+typedef struct {
     Block *blocks;
-};
+    int blocksCount = 0;
+} Func;
 
-struct _var {
+typedef struct {
+    const char *name;
+    void *value;
+} Const;
 
-};
+typedef struct {
+    const char *name;
+    VarType type;
+    void *value;
+} Var;
 
-struct _block {
+// block of instructions
+typedef struct {
+    // label of the block
+    const char *label;
+    // list of instructions
+    Inst *instructions;
+    int instCount = 0;
+} Block;
 
-};
+// instruction
+typedef struct {
+;
+} Inst;
 
 #endif // LUVA_x86_64_ASSEMBLY
