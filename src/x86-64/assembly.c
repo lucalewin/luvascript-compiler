88574b62473ad5ab2e04499cd4debc9ff0f388c4
lucalewin <luca.lewin23@gmail.com>
2021-07-28 15:26:03 +0200

    create assembly.c  - implementation of assembly.h

---
+++ src/x86-64/assembly.c
@@ -0,0 +1,45 @@
+/* 
+ * 
+ * This file implements the prototypes defined in the x86-64/assembly header
+ * 
+ * Docs: https://github.com/lucr4ft/luvascript-compiler/tree/develop/docs/luva-assembly
+ * License: https://github.com/lucr4ft/luvascript-compiler/blob/develop/LICENSE
+ * 
+ * Copyright (c) 2021 - Luca Lewin
+ * 
+ */
+
+// includes
+// --------
+#include <x86-64/assembly.h>
+
+// methods implementations
+// -----------------------
+
+char *asmToString(Assembly *assembly) {
+
+}
+
+char *sectionToString(Section *section) {
+
+}
+
+char *functionToString(Function *func) {
+
+}
+
+char *constToString(Const *c) {
+
+}
+
+char *varToString(Var *var) {
+
+}
+
+char *blockToString(Block *block) {
+
+}
+
+char *instToString(Instruction *inst) {
+
+}
