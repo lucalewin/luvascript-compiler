eb4d97fca52f42556d74ebf734edafe9b49c577f
lucalewin <luca.lewin23@gmail.com>
2021-09-25 13:11:02 +0200

    implement first part of conversion to assembly

--- src/x86-64/parser.c
+++ src/x86-64/parser.c
@@ -1,17 +1,31 @@
+#include <arraylist.h>
 #include <parser.h>
 #include <x86-64/parser.h>
+#include <statement.h>
 
 char *convert_ast_to_x86_64_assembly(NODE *root_node) {
-    
     return "";
 }
 
 char *parser_convert_function_to_assembly(Function *func) {
-    char *template = "global %s\n%s:\n";
-    char *s = calloc(sizeof(template) + 2 * sizeof(func->name), sizeof(char));
-    sprintf(s, template, func->name, func->name);
-    printf("%s\n", s);
-    return s;
+    char *code = "global ";
+
+    stradd(code, func->name);
+    stradd(code, "\n");
+    stradd(code, func->name);
+    stradd(code, ":\n");
+
+    // TODO: load parameters
+
+    for (int i = 0; i < func->statements->size; i++) {
+        Statement *stmt = arraylist_get(func->statements, i);
+        stradd(code, parser_convert_statement_to_assembly(stmt));
+        stradd(code, "\n");
+    }
+
+    stradd(code, "ret\n");
+
+    return code;
 }
 
 char *parser_convert_statement_to_assembly(Statement *stmt) {
@@ -23,15 +37,10 @@ char *parser_convert_expression_to_assembly(Expr* expr) {
 }
 
 char *mov(char *from, char *to);
-char *test(const char* format, ...);
 
 char *mov(char *from, char *to) {
     char *template = "mov %s, %s";
     char *mov = calloc(sizeof(template) + sizeof(from) + sizeof(to), sizeof(char));
     sprintf(mov, template, from, to);
     return mov;
-}
-
-char *test(const char* format, ...) {
-    
 }
