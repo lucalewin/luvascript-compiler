8c0316b7e3fe6a43649f87c51323609fbad9dd03
lucalewin <luca.lewin23@gmail.com>
2021-10-15 15:19:07 +0200

    rewrite expression.h

--- inc/type/expression.h
+++ inc/type/expression.h
@@ -1,96 +1,46 @@
-#ifndef LUVA_EXPR_H
-#define LUVA_EXPR_H
+#ifndef LUVA_EXPRESSION_H
+#define LUVA_EXPRESSION_H
 
-// -----------------------------------------------
+#include <operator.h>
+#include <literal.h>
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+typedef enum ExpressionType ExpressionType;
+typedef struct Expression Expression_T;
+typedef struct UnaryExpression UnaryExpression_T;
+typedef struct BinaryExpression BinaryExpression_T;
+typedef struct NestedExpression NestedExpression_T;
 
-#include "util.h"
-
-// -----------------------------------------------
-
-typedef enum _ExprType expr_t;
-typedef enum _LiteralExprType literal_expr_t;
-
-typedef struct _Expr Expr;
-typedef struct _LiteralExpr LiteralExpr;
-typedef struct _BinaryExpr BinaryExpr;
-typedef struct _UnaryExpr UnaryExpr;
-typedef struct _TernaryExpr TernaryExpr;
-
-// -----------------------------------------------
-
-enum _ExprType {
-    EXPR_BASE,
-    EXPR_LITERAL,
-    EXPR_BINARY,
-    EXPR_UNARY,
-    EXPR_TERNARY
+enum ExpressionType {
+    EXPRESSION_LITERAL,
+    EXPRESSION_UNARY,
+    EXPRESSION_BINARY,
+    EXPRESSION_NESTED
 };
 
-enum _LiteralExprType {
-    IDENTIFIER,
-    STRING,
-    NUMBER
-};
+struct Expression {
+    ExpressionType type;
 
-// -----------------------------------------------
-
-struct _Expr {
-    expr_t type;
     union {
-        Expr *e;
-        LiteralExpr *l;
-        BinaryExpr *b;
-        UnaryExpr *u;
-        TernaryExpr *t;
-    };
+        Literal_T *literal_expr;
+        UnaryExpression_T *unary_expr;
+        BinaryExpression_T *binary_expr;
+        NestedExpression_T *nested_expr;
+    } expr;
 };
 
-Expr *literal_expr_to_expr(LiteralExpr *literal_expr);
-Expr *binary_expr_to_expr(BinaryExpr *binary_expr);
-Expr *unary_expr_to_expr(UnaryExpr *unary_expr);
-
-void print_expr(Expr *expr);
-
-// -----------------------------------------------
-
-struct _LiteralExpr {
-    literal_expr_t type;
-    union {
-        char *identifier;
-        char *string;
-        int number;
-    };
+struct UnaryExpression {
+    Literal_T *identifier;
+    UnaryOperator_T *operator;
 };
-LiteralExpr *literal_expr_create(literal_expr_t type, char *data);
 
-// -----------------------------------------------
-
-struct _BinaryExpr {
-    Expr *left;
-    char *op;
-    Expr *right;
+struct BinaryExpression {
+    Expression_T *expression_left;
+    BinaryOperator operator;
+    Expression_T *expression_right;
 };
-BinaryExpr *binary_expr_create(Expr *left, char *op, Expr *right);
 
-// -----------------------------------------------
-
-struct _UnaryExpr {
-    char *op;
-    Expr *expr;
+struct NestedExpression {
+    Expression_T *expression;
 };
-UnaryExpr *unary_expr_create(char *op, Expr *expr);
 
-// -----------------------------------------------
-
-struct _TernaryExpr {
-    Expr *condition;
-    Expr *expr_true;
-    Expr *expr_false;
-};
-TernaryExpr *ternary_expr_create(Expr *expr_cond, Expr *expr_true, Expr *expr_false);
-
-#endif // LUVA_EXPR_H
+#endif // LUVA_EXPRESSION_H
