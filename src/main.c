eb2fc36725187060ce05edfbe4e69fab9e2bd1c1
lucalewin <luca.lewin23@gmail.com>
2021-10-15 17:46:41 +0200

    update main function for new parsing approach

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 #include <util.h>
+#include <debug.h>
 #include <logger.h>
 #include <arraylist.h>
 #include <lexer.h>
@@ -22,17 +23,41 @@ int main(int argc, char **argv) {
     // TODO: parse commandline arguments
 
     log_debug("reading file contents from '%s'\n", argv[1]);
+    // read code
     char* file_contents = read_file(argv[1]);
     
+    // tokenize code
     ArrayList *tokens = tokenize(file_contents);
-
-    parse_tokens(tokens);
+    // parse tokens to an ast
+    AST *root = parse(tokens);
     arraylist_free(tokens);
 
-    // int last_index = str_last_index_of(argv[1], '.');   //
-    // char *file_base_name = malloc(last_index);          // get filepath without extension
-    // substring(argv[1], file_base_name, last_index);     //
-    // free(file_base_name);
+    // print ast
+    log_debug("{");
+    print_statement(root->statement);
+    printf("}\n");
+
+    // parse ast to x86_64 assembly 
+    char *asm_code = parse_ast_to_x86_64_asm(root);
+    free(root);
+
+    // create filename of the assembly file
+    char *asm_file_name = calloc(strlen(argv[1]), sizeof (char));
+    stradd(asm_file_name, argv[1]);
+    stradd(asm_file_name, ".asm");
+
+    // create / write asm file
+    write_file(asm_file_name, asm_code);
+    
+    // if (asm != NULL) {
+    //     free(asm);
+    // }
+
+    // compile asm file
+    compile_asm(asm_file_name, DEFAULT_BINARY_NAME);
+    free(asm_file_name);
+
+    log_info("successfully compiled '%s'\n", argv[1]);
 
     return 0;
 }
