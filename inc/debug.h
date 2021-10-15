f1f9d57e3eb1e2e8094f15326d98a8a5add25e15
lucalewin <luca.lewin23@gmail.com>
2021-10-15 15:34:38 +0200

    rewrite debug printer

--- inc/debug.h
+++ inc/debug.h
@@ -1,18 +1,12 @@
 #ifndef LUVA_DEBUG_H
 #define LUVA_DEBUG_H
 
-#include "expression.h"
-#include "statement.h"
-#include "function.h"
+#include <literal.h>
+#include <expression.h>
+#include <statement.h>
 
-void debugPrintTab(int amount);
-
-void debugPrintExpression(Expr *expr, int depth);
-void debugPrintStatement(Statement *st, int depth);
-void debugPrintFuncParam(FuncParam *func_param, int depth);
-void debugPrintFuncReturnType(FuncReturnType *return_type, int depth);
-void debugPrintFunction(Function *func, int depth);
-
-void debugPrintType(DataType *datatype, int depth);
+void print_literal(Literal_T *literal);
+void print_expression(Expression_T *expression);
+void print_statement(Statement *statement);
 
 #endif // LUVA_DEBUG_H
