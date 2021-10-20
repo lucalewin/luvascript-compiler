b220ec571a294ec39512b8017c4c404206b659dd
= <luca.lewin23@gmail.com>
2021-10-20 21:34:40 +0200

    add scope pointer to ast struct

--- inc/types/ast.h
+++ inc/types/ast.h
@@ -1,12 +1,14 @@
 #ifndef LUVA_AST_H
 #define LUVA_AST_H
 
+#include <scope.h>
 #include <types/statement.h>
 
 typedef struct _ast AST;
 
 struct _ast {
-    Statement *statement;
+	Scope *global_scope;
+	Statement *statement;
 };
 
 #endif // LUVA_AST_H
