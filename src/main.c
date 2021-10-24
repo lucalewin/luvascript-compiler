71cf082a571fc18229201eeb037d96f98c3b6e7a
lucalewin <luca.lewin23@gmail.com>
2021-10-24 17:48:51 +0200

    implement commandline argument parsing

--- src/main.c
+++ src/main.c
@@ -4,6 +4,8 @@
 #include <string.h>
 
 // utility functions
+#include <options.h>
+#include <util/cmd.h>
 #include <util/util.h>
 #include <util/arraylist.h>
 #include <util/file.h>
@@ -15,35 +17,41 @@
 #include <parser.h>
 #include <compiler.h>
 
+// default filename of the generated binary file if no output filename is specified
 #define DEFAULT_BINARY_NAME "a.out"
+#define VERSION "0.1.0-alpha"
 
 void print_help();
 
 int main(int argc, char **argv) {
 	// validate command line arguments
 	if (argc < 2) {
-		log_debug("ERROR: invalid argument length: %d\n", argc - 1);
 		log_error("no input file specified\n");
 		log_error("type 'lvc -h' for help\n");
 		return -1;
 	}
 
-	// TODO: parse commandline arguments
+	// parse commandline arguments
+	CommandlineOptions *options = parse_commandline_arguments(argc, argv);
 
-	if (strcmp (argv[1], "-h") == 0) {
-		print_help();
-		return 0;
+	// check if a scource file is specified
+	if (options->input_file_name == NULL) {
+		log_error("no input file specified\n");
+		log_error("type 'lvc -h' for help\n");
+		return -1;
 	}
 
-	// log_debug("reading file contents from '%s'\n", argv[1]);
-	// read code
-	char* file_contents = read_file(argv[1]);
-	if (!file_contents) {
+	// read code from source file
+	char* file_contents = read_file(options->input_file_name);
+	if (file_contents == NULL) {
+		options_free(options);
 		exit(1);
 	}
-    
+
 	// tokenize code
 	ArrayList *tokens = tokenize(file_contents);
+	free(file_contents);
+
 	// parse tokens to an ast
 	AST *root = parse(tokens);
 	arraylist_free(tokens);
@@ -58,20 +66,93 @@ int main(int argc, char **argv) {
 	free(root);
 
 	// create filename of the assembly file
-	char *asm_file_name = stradd(argv[1], ".asm");
+	char *asm_file_name = stradd(options->input_file_name, ".asm");
 
 	// create / write asm file
 	write_file(asm_file_name, asm_code);
 
+	// free allocated memory for asm_code
+	free(asm_code);
+
+	char *assembler = "nasm";
+	char *file_format = "-f elf64";
+	char *linker = "ld";
+
 	// compile asm file
-	compile_asm_file(asm_file_name, DEFAULT_BINARY_NAME);
+	if (options->link) {
+		// `-c` compiler flag is specified
+		char *obj_file = calloc(1, sizeof(char));
+		obj_file = straddall(obj_file, asm_file_name, ".o", NULL);
+		// compile to object file
+		exec(assembler, file_format, asm_file_name, "-o", obj_file, NULL);
+		// link to binary file
+		exec(linker, "-g", obj_file, options->output_file_name, NULL);
+		// remove generated object and assembly files
+		exec("rm", asm_file_name, obj_file, NULL);
+
+		// free allocated memory for char*
+		free(obj_file);
+	} else {
+		// compile to object file
+		exec(assembler, file_format, asm_file_name, "-o", options->output_file_name, NULL);
+
+		// remove generated assembly file
+		exec("rm", asm_file_name, NULL);
+	}
+
 	free(asm_file_name);
 
-	log_info("successfully compiled '%s'\n", argv[1]);
+	log_info("successfully compiled '%s'\n", options->input_file_name);
 
 	return 0;
 }
 
+/**
+ * @brief parses the command line arguments
+ * 
+ * @param argc the number of arguments
+ * @param argv the arguments
+ * @return CommandlineOptions* 
+ */
+CommandlineOptions *parse_commandline_arguments(int argc, char *argv[]) {
+	CommandlineOptions *options = calloc(1, sizeof(CommandlineOptions));
+
+	// assign default values to options
+	options->input_file_name = NULL;
+	options->output_file_name = DEFAULT_BINARY_NAME;
+	options->link = 1; // true
+
+	for (int i = 1; i < argc; i++) {
+		if (strcmp(argv[i], "-c") == 0) { // -c
+			options->link = 0;
+		} else if (strcmp(argv[i], "-o") == 0) { // -o
+			if (i + 1 < argc) {
+				options->output_file_name = calloc(strlen(argv[i + 1]), sizeof(char));
+				strcpy(options->output_file_name, argv[i + 1]);
+				i++;
+			}
+		} else if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) { // -h, --help
+			print_help();
+			exit(0);
+		} else if (strcmp(argv[i], "--version") == 0) { // --version
+			printf("lvc version %s\n", VERSION);
+			exit(0);
+		} else {
+			if (options->input_file_name != NULL) {
+				free(options->input_file_name);
+			}
+			options->input_file_name = calloc(strlen(argv[i]), sizeof(char));
+			strcpy(options->input_file_name, argv[i]);
+		}
+	}
+
+	return options;
+}
+
+/**
+ * @brief 
+ * TODO: add help for compiler flags
+ */
 void print_help() {
 	printf(YELLOW "usage" RESET ": lvc <inputfile>\n");
 }
