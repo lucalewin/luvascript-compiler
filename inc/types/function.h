03db69ff5243a70422d82376a334672da7dfe57a
lucalewin <luca.lewin23@gmail.com>
2021-10-24 17:44:44 +0200

    create struct FunctionTemplate + change type of Function::return_type to Datatype

--- inc/types/function.h
+++ inc/types/function.h
@@ -3,14 +3,23 @@
 
 #include <util/arraylist.h>
 #include <types/statement.h>
+#include <types/datatypes.h>
 
 typedef struct _function {
 	char *identifier;
 	ArrayList *parameters;
-	int return_type;
+	int return_type_old;
+	Datatype *return_type;
 	Scope *global_scope;
 	Scope *local_scope;
 	ArrayList *body_statements;
+	unsigned int is_extern : 1;
 } Function;
+
+typedef struct _func_template {
+	char *identifier;
+	ArrayList *param_datatypes;
+	Datatype *return_type;
+} FunctionTemplate;
 
 #endif // LUVA_TYPES_FUNCTION_H
