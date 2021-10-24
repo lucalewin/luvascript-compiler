4df9b5fd4dd33388e6d230a36b9946f68c31f3da
lucalewin <luca.lewin23@gmail.com>
2021-10-24 17:44:06 +0200

    change datatype filed to struct Datatype

--- inc/types/variable.h
+++ inc/types/variable.h
@@ -1,15 +1,17 @@
 #ifndef LUVA_TYPE_VARIABLE_H
 #define LUVA_TYPE_VARIABLE_H
 
+#include <types/datatypes.h>
 #include <types/literal.h>
 #include <types/expression.h>
 
 typedef struct _Variable Variable;
 
 struct _Variable {
-	int datatype;
+	Datatype *datatype;
 	Literal_T *identifier;
 	Expression_T *default_value;
+	unsigned int is_constant : 1;
 };
 
 #endif // LUVA_TYPE_VARIABLE_H
