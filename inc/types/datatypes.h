ff3aae4de8a1eef7833a14a48d3b4f76ce0def63
lucalewin <luca.lewin23@gmail.com>
2021-10-24 17:43:06 +0200

    create struct Datatype + implement parsing for primitive datatypes

---
+++ inc/types/datatypes.h
@@ -0,0 +1,15 @@
+#ifndef LUVA_TYPES_DATATYPES_H
+#define LUVA_TYPES_DATATYPES_H
+
+#include <string.h>
+#include <logging/logger.h>
+
+typedef struct _datatype {
+	char *type_identifier;
+	unsigned int size;
+	unsigned int is_primitive : 1;
+} Datatype;
+
+Datatype *parse_datatype(char *type);
+
+#endif
---
+++ src/datatypes.c
@@ -0,0 +1,35 @@
+#include <types/datatypes.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+Datatype *parse_datatype(char *type) {
+	Datatype *dt = calloc(1, sizeof(Datatype));
+
+	if (strcmp(type, "byte") == 0) {
+		dt->is_primitive = 1;
+		dt->type_identifier = "byte";
+		dt->size = 1; // 1 byte
+	} else if (strcmp(type, "short") == 0) {
+		dt->is_primitive = 1;
+		dt->type_identifier = "short";
+		dt->size = 2; // 2 bytes
+	} else if (strcmp(type, "int") == 0) {
+		dt->is_primitive = 1;
+		dt->type_identifier = "int";
+		dt->size = 4; // 4 bytes
+	} else if (strcmp(type, "long") == 0) {
+		dt->is_primitive = 1;
+		dt->type_identifier = "long";
+		dt->size = 8; // 8 bytes
+	} else if (strcmp(type, "bool") == 0) {
+		dt->is_primitive = 1;
+		dt->type_identifier = "bool";
+		dt->size = 1; // 1 byte
+	} else {
+		log_error("unknown datatype: '%s'\n", type);
+		return NULL;
+	}
+
+	return dt;
+}
