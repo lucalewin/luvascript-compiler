14e5fc48e84053bc36ddae51cad7ccb86f1f1bbd
lucalewin <luca.lewin23@gmail.com>
2021-10-24 17:48:31 +0200

    create struct + method prototypes for commandline argument parsing

---
+++ inc/options.h
@@ -0,0 +1,22 @@
+#ifndef LUVA_OPTIONS_H
+#define LUVA_OPTIONS_H
+
+typedef struct commandline_options {
+	char *input_file_name;
+	char *output_file_name;
+	unsigned int link : 1;
+} CommandlineOptions;
+
+CommandlineOptions *parse_commandline_arguments(int argc, char *argv[]);
+
+void options_free(CommandlineOptions *options);
+
+// implementations
+
+void options_free(CommandlineOptions *options) {
+	free(options->input_file_name);
+	free(options->output_file_name);
+	free(options);
+}
+
+#endif
