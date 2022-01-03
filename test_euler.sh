aff60021c67edc733cca4dde5c16b0cd490cc418
Luca Lewin <luca.lewin23@gmail.com>
2022-01-03 16:29:47 +0100

    create new tests with problems from project euler

---
+++ test_euler.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+echo ""
+echo "--------------- [BUILDING] ---------------"
+echo ""
+
+# build the project
+make build
+
+cd ./tests/euler
+
+if [ $# -eq 0 ]; then
+	echo "No test specified, running all tests"
+	
+	index=1
+
+	# loop through all the test files and run the generated binary file
+	for file in ./*.lvs
+	do
+		echo ""
+		echo "---------------- [Test $index] ----------------"
+		echo ""
+
+		# compile the test file with the previously built project binary
+		../../bin/lvc -S "$file"
+
+		# execute the generated binary file
+		./a.out
+
+		# check if the test passed or failed
+		if [ $? -eq 0 ]; then
+			echo "Test $index passed"
+		else
+			echo "Test $index failed with exit code $?"
+		fi
+
+		index=$((index+1));
+	done
+else
+	for test in $@; do
+		echo ""
+		echo "---------------- [Test $test] ----------------"
+		echo ""
+
+		# compile the test file with the previously built project binary
+		../../bin/lvc -S problem${test}.lvs
+
+		# execute the generated binary file
+		./a.out
+
+		# check if the test passed or failed
+		if [ $? -eq 0 ]; then
+			echo "Test $index passed"
+		else
+			echo "Test $index failed with exit code $?"
+		fi
+	done
+fi
+
+# remove generated binary files for the test files
+rm -rf ./*.o ./*.out
