8c6f1404c6b843eb8804b9ff5936147553c7ec24
Luca Lewin <luca.lewin23@gmail.com>
2022-01-05 20:59:15 +0100

    move `options_free` into seperate source file

--- inc/options.h
+++ inc/options.h
@@ -1,9 +1,13 @@
 #ifndef LUVA_OPTIONS_H
 #define LUVA_OPTIONS_H
 
+#include <util/arraylist.h>
+
 typedef struct commandline_options {
 	char *input_file_name;
+	ArrayList *source_files;
 	char *output_file_name;
+	ArrayList *library_paths;
 	unsigned int link : 1;
 	unsigned int generate_assembly : 1;
 } CommandlineOptions;
@@ -11,13 +15,5 @@ typedef struct commandline_options {
 CommandlineOptions *parse_commandline_arguments(int argc, char *argv[]);
 
 void options_free(CommandlineOptions *options);
-
-// implementations
-
-void options_free(CommandlineOptions *options) {
-	free(options->input_file_name);
-	free(options->output_file_name);
-	free(options);
-}
 
 #endif
---
+++ src/options.c
@@ -0,0 +1,97 @@
+#include <options.h>
+
+#include <string.h>
+
+#include <util/util.h>
+#include <logging/logger.h>
+
+// default filename of the generated binary file if no output filename is specified
+#define DEFAULT_BINARY_NAME "a.out"
+#define VERSION "0.1.0-alpha"
+
+void print_help();
+
+/**
+ * @brief parses the command line arguments
+ * 
+ * @param argc the number of arguments
+ * @param argv the arguments
+ * @return CommandlineOptions* 
+ */
+CommandlineOptions *parse_commandline_arguments(int argc, char *argv[]) {
+	CommandlineOptions *options = calloc(1, sizeof(CommandlineOptions));
+
+	// assign default values to options
+	options->input_file_name = NULL;
+	options->source_files = arraylist_create();
+	options->output_file_name = NULL;
+	options->library_paths = arraylist_create();
+	options->link = 1; // true
+	options->generate_assembly = 0; // false
+
+	// parse command line arguments
+	for (size_t i = 1; i < argc; i++) {
+		if (argv[i][0] == '-') {
+			// parse command line arguments
+			if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
+				print_help();
+				options_free(options);
+				exit(0);
+			} else if (strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--version") == 0) {
+				printf("lvc version %s\n", VERSION);
+				exit(0);
+			} else if (strcmp(argv[i], "-o") == 0) {
+				// output file name
+				if (i + 1 < argc) {
+					options->output_file_name = argv[i + 1];
+					i++;
+				} else {
+					log_error("no output file specified\n");
+					log_error("type 'lvc -h' for help\n");
+					options_free(options);
+					exit(1);
+				}
+			} else if (strcmp(argv[i], "-S") == 0) {
+				// generate assembly
+				options->generate_assembly = 1;
+			} else if (strcmp(argv[i], "-c") == 0) {
+				// compile to object file
+				options->link = 0;
+			} else if (strcmp(argv[i], "-lib") == 0) {
+				if (i + 1 < argc) {
+					// log_debug("adding library path '%s'\n", argv[i + 1]);
+					arraylist_add(options->library_paths, argv[i + 1]);
+					i++;
+				} else {
+					log_error("no library path specified\n");
+					log_error("type 'lvc --help:-lib' for help\n");
+					options_free(options);
+					exit(1);
+				}
+			} else {
+				log_error("unknown option '%s'\n", argv[i]);
+				log_error("type 'lvc -h' for help\n");
+				options_free(options);
+				exit(1);
+			}
+		} else {
+			// input file name
+			// options->input_file_name = argv[i];
+			arraylist_add(options->source_files, argv[i]);
+		}
+	}
+
+	if (options->output_file_name == NULL) {
+		options->output_file_name = strdup(DEFAULT_BINARY_NAME);
+	}
+
+	return options;
+}
+
+/**
+ * @brief 
+ * TODO: add help for compiler flags
+ */
+void print_help() {
+	printf(YELLOW "usage" RESET ": lvc <inputfile>\n");
+}
