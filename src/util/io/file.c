73adf27856a44f635226104ca46544d6fe587118
Luca Lewin <luca.lewin23@gmail.com>
2022-01-15 16:11:01 +0100

    add new utililty functions

--- src/util/io/file.c
+++ src/util/io/file.c
@@ -2,7 +2,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+#include <dirent.h>
+
+#include <util/arraylist.h>
 #include <logging/logger.h>
 
 char *read_file(const char* path) {
@@ -34,4 +38,46 @@ void write_file(const char* path, char *text) {
     }
     fputs(text, file);
     fclose(file);
+}
+
+int file_exists(const char *path) {
+	FILE *file = fopen(path, "r");
+	if (file) {
+		fclose(file);
+		return 1;
+	}
+	return 0;
+}
+
+// list all files in the current directory
+ArrayList *list_files(const char *directory) {
+	ArrayList *files = arraylist_create();
+	DIR *dir = opendir(directory);
+	struct dirent *entry;
+	while ((entry = readdir(dir)) != NULL) {
+		if (entry->d_type == 0x08) { // _A_VOLID = 0x08 --> file
+			arraylist_add(files, entry->d_name);
+		}
+	}
+	closedir(dir);
+	return files;
+}
+
+ArrayList *list_dirs(const char *directory) {
+	ArrayList *dirs = arraylist_create();
+	DIR *dir = opendir(directory);
+	struct dirent *entry;
+	while ((entry = readdir(dir)) != NULL) {
+		if (entry->d_type == 0x04) { // directory
+			arraylist_add(dirs, entry->d_name);
+		}
+	}
+	closedir(dir);
+	return dirs;
+}
+
+const char *get_filename_extension(const char *filename) {
+	const char *dot = strrchr(filename, '.');
+	if(!dot || dot == filename) return "";
+	return dot + 1;
 }
