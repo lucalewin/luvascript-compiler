b9f0f5a3e97f15e00ec37bddb3a74da54d4bb85f
Luca Lewin <luca.lewin23@gmail.com>
2022-01-15 16:23:35 +0100

    add module loading

--- src/main.c
+++ src/main.c
@@ -1,26 +1,24 @@
-// standard libraries
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-// utility functions
 #include <options.h>
 #include <scope_impl.h>
-#include <util/cmd.h>
 #include <util/util.h>
 #include <util/arraylist.h>
 #include <util/file.h>
-#include <logging/debug.h>
 #include <logging/logger.h>
 
-// types
 #include <types/package.h>
 #include <types/ast.h>
 
-// core library functions
 #include <lexer.h>
 #include <parser.h>
+#include <generator.h>
 #include <compiler.h>
+
+// directory of the globaly installed libraries for luvascript
+#define GLOBAL_LIBRARY_DIR "/usr/lib/luva"
 
 int main(int argc, char **argv) {
 	// validate command line arguments
@@ -40,11 +38,23 @@ int main(int argc, char **argv) {
 	}
 
 	// compile the source files
-	AST *ast = calloc(1, sizeof(AST));
-	ast->packages = arraylist_create();
+	AST *ast = ast_new();
 
 	for (size_t i = 0; i < options->source_files->size; i++) {
 		char *source_file_name = arraylist_get(options->source_files, i);
+
+		if (!file_exists(source_file_name)) {
+			log_error("source file '%s' does not exist\n", source_file_name);
+			// free allocated memory
+			for (size_t j = 0; j < ast->packages->size; j++) {
+				Package *package = arraylist_get(ast->packages, j);
+				package_free(package);
+			}
+			ast_free(ast);
+			options_free(options);
+			return -1;
+		}
+
 		char *source_code = read_file(source_file_name);
 		
 		// log_debug("parsing source file %s\n", arraylist_get(options->source_files, i));
@@ -60,6 +70,34 @@ int main(int argc, char **argv) {
 		for (size_t i = 0; i < tokens->size; i++)
 			token_free(arraylist_get(tokens, i));
 		arraylist_free(tokens);
+	}
+
+	// load global libraries
+	ArrayList *global_libraries_dir_module_dirs = list_dirs(GLOBAL_LIBRARY_DIR);
+	// print filename of global libraries
+	for (size_t i = 2; i < global_libraries_dir_module_dirs->size; i++) {
+		char *module_dir = arraylist_get(global_libraries_dir_module_dirs, i);
+		log_debug("loading global module: %s\n", module_dir);
+
+		// load all files in the module directory
+		ArrayList *module_dir_files = list_files(module_dir);
+		// extract all header files
+		for (size_t j = 0; j < module_dir_files->size; j++) {
+			char *file_name = arraylist_get(module_dir_files, j);
+			if (strcmp(get_filename_extension(file_name), "lvh") == 0) {
+				char *header_file_name = concat(module_dir, "/", file_name);
+				char *header_file_source_code = read_file(header_file_name);
+				ArrayList *header_file_tokens = tokenize(header_file_source_code);
+				// Package *header_file_package = parse(header_file_tokens);
+				AST *module_ast = parse_header(header_file_tokens);
+				// arraylist_add(ast->packages, header_file_package);
+				free(header_file_source_code);
+				for (size_t i = 0; i < header_file_tokens->size; i++)
+					token_free(arraylist_get(header_file_tokens, i));
+				arraylist_free(header_file_tokens);
+				free(header_file_name);
+			}
+		}
 	}
 
 	// if packages have the same name, merge them
@@ -84,7 +122,19 @@ int main(int argc, char **argv) {
 	}
 
 	// generate assembly code
-	char *assembly_code = compile(ast, options);
+	char *assembly_code = generate_assembly(ast, options); //compile(ast, options);
+
+	if (options->is_library) {
+		// generate header for library
+		char *header_code = generate_header(ast, options);
+		// write header to file
+		char *header_file_name = malloc(strlen(options->output_file_name) + 5);
+		strcpy(header_file_name, options->output_file_name);
+		strcat(header_file_name, ".lvh");
+		write_file(header_file_name, header_code);
+		free(header_file_name);
+		free(header_code);
+	}
 
 	if (options->output_file_name == NULL) {
 		log_error("output_file_name is not specified\n");
@@ -100,38 +150,7 @@ int main(int argc, char **argv) {
 	// write assembly code to file
 	write_file(asm_file_name, assembly_code);
 
-	char *assembler = "nasm";
-	char *file_format = "-f elf64";
-	char *linker = "ld";
-
-	// compile asm file
-	if (options->link) {
-		// `-c` compiler flag is specified
-		char *obj_file = calloc(1, sizeof(char));
-		obj_file = straddall(obj_file, asm_file_name, ".o", NULL);
-		// compile to object file
-		exec(assembler, file_format, asm_file_name, "-o", obj_file, NULL);
-		// link to binary file
-		exec(linker, "-g", obj_file, "-o", options->output_file_name, NULL);
-		// remove generated assembly file
-		if (!options->generate_assembly) {
-			exec("rm", asm_file_name, NULL);
-		}
-
-		// remove object file
-		exec("rm", obj_file, NULL);
-
-		// free allocated memory for char*
-		free(obj_file);
-	} else {
-		// compile to object file
-		exec(assembler, file_format, asm_file_name, "-o", options->output_file_name, NULL);
-
-		// remove generated assembly file
-		if (!options->generate_assembly) {
-			exec("rm", asm_file_name, NULL);
-		}
-	}
+	compile(asm_file_name, options);
 
 	free(asm_file_name);
 
