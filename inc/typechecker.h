67fabc83c6356578358a9688c096d703855b070e
Luca Lewin <luca.lewin23@gmail.com>
2022-04-04 21:20:41 +0200

    move function declaration into source file

--- inc/typechecker.h
+++ inc/typechecker.h
@@ -13,20 +13,4 @@
  */
 bool check_types(AST *ast);
 
-/**
- * @brief checks if the given ast contains duplicate functions
- * 
- * @param ast the current ast
- * @return true if duplicate functions were found, false otherwise
- */
-bool check_for_duplicate_functions(AST *ast);
-
-/**
- * @brief checks if the given ast contains duplicate variables
- * 
- * @param ast the current ast
- * @return true if duplicate variables were found, false otherwise
- */
-bool check_for_duplicate_variables(AST *ast);
-
 #endif // __LUVASCRIPT_TYPECHECKER_H__
