82d0819c76fa82e3fe964d0a64607efb54f0b3be
Luca Lewin <luca.lewin23@gmail.com>
2022-04-05 20:45:19 +0200

    implemented checks for unreachable code paths

--- src/typechecker.c
+++ src/typechecker.c
@@ -7,6 +7,7 @@
 #include <types/variable.h>
 
 #include <logging/logger.h>
+#include <logging/debug.h>
 
 /**
  * @brief checks if the given ast contains duplicate functions
@@ -14,7 +15,7 @@
  * @param ast the current ast
  * @return true if duplicate functions were found, false otherwise
  */
-bool check_for_duplicate_functions(AST *ast);
+bool duplicate_functions_exist(AST *ast);
 
 /**
  * @brief checks if the given ast contains duplicate variables
@@ -22,7 +23,7 @@ bool check_for_duplicate_functions(AST *ast);
  * @param ast the current ast
  * @return true if duplicate variables were found, false otherwise
  */
-bool check_for_duplicate_variables(AST *ast);
+bool duplicate_variables_exist(AST *ast);
 
 /**
  * @brief TODO(lucalewin) add description
@@ -30,7 +31,7 @@ bool check_for_duplicate_variables(AST *ast);
  * @param ast 
  * @return true 
  */
-bool check_if_all_code_paths_return(AST *ast);
+bool all_code_paths_return(AST *ast);
 
 /**
  * @brief TODO(lucalewin) add description
@@ -38,7 +39,7 @@ bool check_if_all_code_paths_return(AST *ast);
  * @param ast 
  * @return true 
  */
-bool check_if_all_code_blocks_are_reachable(AST *ast);
+bool all_code_blocks_are_reachable(AST *ast);
 
 // utility functions
 
@@ -50,21 +51,33 @@ bool check_if_all_code_blocks_are_reachable(AST *ast);
  */
 bool check_if_branch_retuns(ConditionalStatement *conditional_stmt);
 
+/**
+ * @brief TODO(lucalewin) add description
+ * 
+ * @param conditional_stmt 
+ * @return true 
+ */
+bool branch_contains_unreachable_code(ConditionalStatement *conditional_stmt);
+
 // ----------------------------------------------------------------
 
 bool check_types(AST *ast) {
 	log_info("Starting type-checking...\n");
 
-	if (check_for_duplicate_functions(ast)) {
+	if (duplicate_functions_exist(ast)) {
 		return false;
 	}
 
-	if (check_for_duplicate_variables(ast)) {
+	if (duplicate_variables_exist(ast)) {
 		return false;
 	}
 
-	if (!check_if_all_code_paths_return(ast)) {
+	if (!all_code_paths_return(ast)) {
 		return false;
+	}
+
+	if (!all_code_blocks_are_reachable(ast)) {
+		// warning is already printed
 	}
 
 	log_info("Finnished type-checking\n");
@@ -75,7 +88,7 @@ bool check_types(AST *ast) {
 // implementation
 // ----------------------------------------------------------------
 
-bool check_for_duplicate_functions(AST *ast) {
+bool duplicate_functions_exist(AST *ast) {
 	log_debug("Checking for duplicate functions\n");
 
 	bool duplicated_functions_found = false;
@@ -134,7 +147,7 @@ bool check_for_duplicate_functions(AST *ast) {
 	return duplicated_functions_found;
 }
 
-bool check_for_duplicate_variables(AST *ast) {
+bool duplicate_variables_exist(AST *ast) {
 	log_debug("Checking for duplicate variables\n");
 
 	bool duplicated_variables_found = false;
@@ -171,7 +184,7 @@ bool check_for_duplicate_variables(AST *ast) {
 	return duplicated_variables_found;
 }
 
-bool check_if_all_code_paths_return(AST *ast) {
+bool all_code_paths_return(AST *ast) {
 	log_debug("Checking if all code paths return\n");
 
 	bool all_code_paths_return = true;
@@ -182,9 +195,10 @@ bool check_if_all_code_paths_return(AST *ast) {
 		for (int j = 0; j < arraylist_size(package->functions); j++) {
 			Function *function = arraylist_get(package->functions, j);
 
-			// if the function returns `void` then no return statement is required
-			if (strcmp(function->return_type->type_identifier, "void") == 0) {
-				continue;
+			if (arraylist_size(function->body_statements) == 0) {
+				printf("    " YELLOW "warning: " RESET "function '%s' (%s:%d:%d) has an empty body\n", 
+						function->identifier,
+						"TODO", 0, 0);
 			}
 
 			// loop through all statements in the function
@@ -212,6 +226,22 @@ bool check_if_all_code_paths_return(AST *ast) {
 
 			}
 			// if this is reached, not all code paths return
+
+			// if the function returns `void` then no return statement is required
+			// but internally we need to have a return statement, otherwise a
+			// `SEGFAULT` occurs when the function is called
+			if (strcmp(function->return_type->type_identifier, "void") == 0) {
+
+				Statement *statement = calloc(1, sizeof(Statement));
+				statement->scope = function->scope;
+				statement->type = STATEMENT_RETURN;
+				statement->stmt.return_statement = calloc(1, sizeof(ReturnStatement));
+				statement->stmt.return_statement->expression = NULL;
+
+				arraylist_add(function->body_statements, statement);
+				goto next_function;
+			}
+
 			printf("    " IRED "error: " RESET "function '%s' (%s:%d:%d) does not return\n", 
 					function->identifier,
 					"TODO", 0, 0);
@@ -225,10 +255,80 @@ bool check_if_all_code_paths_return(AST *ast) {
 	return all_code_paths_return;
 }
 
-bool check_if_all_code_blocks_are_reachable(AST *ast) {
-	
+/**
+ * If a path cannot be reached, the compiler will
+ * remove the unreachable code.
+ */
+bool all_code_blocks_are_reachable(AST *ast) {
+	log_debug("Checking if all code paths are reachable\n");
+	// TODO(lucalewin): implement
 
-	return false;
+	bool all_code_paths_return = true;
+
+	for (size_t i = 0; i < arraylist_size(ast->packages); i++) {
+		Package *package = arraylist_get(ast->packages, i);
+
+		for (size_t j = 0; j < arraylist_size(package->functions); j++) {
+			Function *function = arraylist_get(package->functions, j);
+
+			for (size_t k = 0; k < arraylist_size(function->body_statements); k++) {
+				Statement *statement = arraylist_get(function->body_statements, k);
+
+				if (statement->type == STATEMENT_CONDITIONAL) {
+					ConditionalStatement *conditional_statement = statement->stmt.conditional_statement;
+
+					branch_contains_unreachable_code(conditional_statement);
+
+					if (check_if_branch_retuns(conditional_statement)) {
+						// check if more statements exist after the branch
+						if (k + 1 < arraylist_size(function->body_statements)) {
+							// there are statements after the conditional statement
+							// which cannot be reached because every branch of the
+							// conditional statement returns
+							printf("    " YELLOW "warning: " RESET "function '%s' (%s:%d:%d) contains unreachable code\n", 
+									function->identifier,
+									"TODO", 0, 0);
+							
+							all_code_paths_return = false;
+
+							// remove unreachable code
+							for (size_t l = arraylist_size(function->body_statements) - 1; l > k; l--)
+							{
+								Statement *statement = arraylist_get(function->body_statements, l);
+								statement_free(statement);
+								arraylist_remove_at_index(function->body_statements, l);
+							}
+						}
+					}
+				} else if (statement->type == STATEMENT_RETURN) {
+					// check if more statements exist after the return statement
+					if (k + 1 < arraylist_size(function->body_statements)) {
+						// there are statements after the return statement
+						// which cannot be reached because the function returns
+						printf("    " YELLOW "warning: " RESET "function '%s' (%s:%d:%d) contains unreachable code\n", 
+								function->identifier,
+								"TODO", 0, 0);
+						
+						all_code_paths_return = false;
+
+						// remove unreachable code
+						for (size_t l = arraylist_size(function->body_statements) - 1; l > k; l--)
+						{
+							Statement *statement = arraylist_get(function->body_statements, l);
+							statement_free(statement);
+							arraylist_remove_at_index(function->body_statements, l);
+						}
+					}
+				} else if (statement->type == STATEMENT_LOOP) {
+					// check if loop condition is always true
+					// if it is, then check if the loop breaks
+					// else the code after 
+				}
+			}
+		}
+	}
+
+	return all_code_paths_return;
 }
 
 // ----------------------------------------------------------------
@@ -275,7 +375,7 @@ bool check_if_branch_retuns(ConditionalStatement *conditional_stmt) {
 		} else if (conditional_stmt->false_branch->type == STATEMENT_CONDITIONAL) {
 			false_branch_retuns = check_if_branch_retuns(conditional_stmt->false_branch->stmt.conditional_statement);
 		} else if (conditional_stmt->false_branch->type == STATEMENT_COMPOUND) {
-			CompoundStatement *compound_statement = conditional_stmt->true_branch->stmt.compound_statement;
+			CompoundStatement *compound_statement = conditional_stmt->false_branch->stmt.compound_statement;
 
 			for (int i = 0; i < arraylist_size(compound_statement->nested_statements); i++) {
 				Statement *statement = arraylist_get(compound_statement->nested_statements, i);
@@ -286,8 +386,11 @@ bool check_if_branch_retuns(ConditionalStatement *conditional_stmt) {
 					ConditionalStatement *conditional_statement = statement->stmt.conditional_statement;
 
 					// TODO: add checks
-					false_branch_retuns = check_if_branch_retuns(conditional_statement);
-					break;
+					// false_branch_retuns = check_if_branch_retuns(conditional_statement);
+					if (check_if_branch_retuns(conditional_statement)) {
+						false_branch_retuns = true;
+						break;
+					}
 				}
 
 				// check if statement is a return statement
@@ -300,4 +403,165 @@ bool check_if_branch_retuns(ConditionalStatement *conditional_stmt) {
 	}
 
 	return true_branch_retuns && false_branch_retuns;
+}
+
+bool branch_contains_unreachable_code(ConditionalStatement *conditional_stmt) {
+
+	bool true_branch_contains_unreachable_code = false, false_branch_contains_unreachable_code = false;
+
+	// check if true branch contains unreachable code
+	if (conditional_stmt->true_branch->type == STATEMENT_CONDITIONAL) {
+		ConditionalStatement *true_branch_conditional_stmt = conditional_stmt->true_branch->stmt.conditional_statement;
+
+		if (branch_contains_unreachable_code(true_branch_conditional_stmt)) {
+			true_branch_contains_unreachable_code = true;
+		}
+	} else if (conditional_stmt->true_branch->type == STATEMENT_COMPOUND) {
+		CompoundStatement *compound_statement = conditional_stmt->true_branch->stmt.compound_statement;
+
+		for (size_t k = 0; k < arraylist_size(compound_statement->nested_statements); k++) {
+			Statement *statement = arraylist_get(compound_statement->nested_statements, k);
+
+			if (statement->type == STATEMENT_CONDITIONAL) {
+				ConditionalStatement *conditional_statement = statement->stmt.conditional_statement;
+
+				if (branch_contains_unreachable_code(conditional_statement)) {
+					true_branch_contains_unreachable_code = true;
+				}
+
+				if (check_if_branch_retuns(conditional_statement)) {
+					// check if more statements exist after the branch
+					if (k + 1 < arraylist_size(compound_statement->nested_statements)) {
+						// there are statements after the conditional statement
+						// which cannot be reached because every branch of the
+						// conditional statement returns
+						printf("    " YELLOW "warning: " RESET "function '%s' (%s:%d:%d) contains unreachable code\n", 
+								//function->identifier,
+								"TODO",
+								"TODO", 0, 0);
+						
+						true_branch_contains_unreachable_code = true;
+
+						// TODO(lucalewin): remove unreachable code
+						for (size_t l = arraylist_size(compound_statement->nested_statements) - 1; l > k; l--)
+						{
+							Statement *statement = arraylist_get(compound_statement->nested_statements, l);
+							statement_free(statement);
+							arraylist_remove_at_index(compound_statement->nested_statements, l);
+						}
+					}
+				}
+			} else if (statement->type == STATEMENT_RETURN) {
+				// check if more statements exist after the return statement
+				if (k + 1 < arraylist_size(compound_statement->nested_statements)) {
+					// there are statements after the return statement
+					// which cannot be reached because the function returns
+					printf("    " YELLOW "warning: " RESET "function '%s' (%s:%d:%d) contains unreachable code\n", 
+							//function->identifier,
+							"TODO",
+							"TODO", 0, 0);
+					
+					true_branch_contains_unreachable_code = true;
+
+					// TODO(lucalewin): remove unreachable code
+					for (size_t l = arraylist_size(compound_statement->nested_statements) - 1; l > k; l--)
+					{
+						Statement *statement = arraylist_get(compound_statement->nested_statements, l);
+						statement_free(statement);
+						arraylist_remove_at_index(compound_statement->nested_statements, l);
+					}
+				}
+			} else if (statement->type == STATEMENT_LOOP) {
+				// TODO(lucalewin): check if loop condition is always true
+				// if it is, then check if the loop breaks
+				// else the code after 
+			}
+		}
+	} else if (conditional_stmt->true_branch->type == STATEMENT_LOOP) {
+		// TODO(lucalewin): check if loop condition is always true
+		// if it is, then check if the loop breaks
+		// else the code after 
+	} // else: statement is not a branch
+
+	// check if false branch contains unreachable code
+	// check if false branch exists
+	if (conditional_stmt->false_branch != NULL) {
+		// check if false branch returns
+		if (conditional_stmt->false_branch->type == STATEMENT_RETURN) {
+			false_branch_contains_unreachable_code = true;
+		} else if (conditional_stmt->false_branch->type == STATEMENT_CONDITIONAL) {
+			ConditionalStatement *false_branch_conditional_stmt = conditional_stmt->false_branch->stmt.conditional_statement;
+
+			if (branch_contains_unreachable_code(false_branch_conditional_stmt)) {
+				false_branch_contains_unreachable_code = true;
+			}
+		} else if (conditional_stmt->false_branch->type == STATEMENT_COMPOUND) {
+			CompoundStatement *compound_statement = conditional_stmt->false_branch->stmt.compound_statement;
+
+			for (size_t k = 0; k < arraylist_size(compound_statement->nested_statements); k++) {
+				Statement *statement = arraylist_get(compound_statement->nested_statements, k);
+
+				if (statement->type == STATEMENT_CONDITIONAL) {
+					ConditionalStatement *conditional_statement = statement->stmt.conditional_statement;
+
+					if (branch_contains_unreachable_code(conditional_statement)) {
+						false_branch_contains_unreachable_code = true;
+					}
+
+					if (check_if_branch_retuns(conditional_statement)) {
+						// check if more statements exist after the branch
+						if (k + 1 < arraylist_size(compound_statement->nested_statements)) {
+							// there are statements after the conditional statement
+							// which cannot be reached because every branch of the
+							// conditional statement returns
+							printf("    " YELLOW "warning: " RESET "function '%s' (%s:%d:%d) contains unreachable code\n", 
+									//function->identifier,
+									"TODO",
+									"TODO", 0, 0);
+							
+							false_branch_contains_unreachable_code = true;
+
+							// TODO(lucalewin): remove unreachable code
+							for (size_t l = arraylist_size(compound_statement->nested_statements) - 1; l > k; l--)
+							{
+								Statement *statement = arraylist_get(compound_statement->nested_statements, l);
+								statement_free(statement);
+								arraylist_remove_at_index(compound_statement->nested_statements, l);
+							}
+						}
+					}
+				} else if (statement->type == STATEMENT_RETURN) {
+					// check if more statements exist after the return statement
+					if (k + 1 < arraylist_size(compound_statement->nested_statements)) {
+						// there are statements after the return statement
+						// which cannot be reached because the function returns
+						printf("    " YELLOW "warning: " RESET "function '%s' (%s:%d:%d) contains unreachable code\n", 
+								//function->identifier,
+								"TODO",
+								"TODO", 0, 0);
+						
+						false_branch_contains_unreachable_code = true;
+
+						// TODO(lucalewin): remove unreachable code
+						for (size_t l = arraylist_size(compound_statement->nested_statements) - 1; l > k; l--)
+						{
+							Statement *statement = arraylist_get(compound_statement->nested_statements, l);
+							statement_free(statement);
+							arraylist_remove_at_index(compound_statement->nested_statements, l);
+						}
+					}
+				} else if (statement->type == STATEMENT_LOOP) {
+					// TODO(lucalewin): check if loop condition is always true
+					// if it is, then check if the loop breaks
+					// else the code after 
+				}
+			}
+		} else if (conditional_stmt->false_branch->type == STATEMENT_LOOP) {
+			// TODO(lucalewin): check if loop condition is always true
+			// if it is, then check if the loop breaks
+			// else the code after 
+		} // else: statement is not a branch
+	}
+
+	return true_branch_contains_unreachable_code || false_branch_contains_unreachable_code;
 }
