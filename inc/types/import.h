d401d921e4e893104fb08db3bab767f18dbfc695
Luca Lewin <luca.lewin23@gmail.com>
2022-04-15 18:38:59 +0200

    remake compact function + add error messages + add utility functions

--- inc/types/import.h
+++ inc/types/import.h
@@ -2,6 +2,7 @@
 #define __LUVA_IMPORT_H__
 
 #include <util/arraylist.h>
+#include <stdbool.h>
 
 typedef struct _ImportDeclaration ImportDeclaration;
 
@@ -11,6 +12,7 @@ typedef struct _ImportDeclaration ImportDeclaration;
  */
 struct _ImportDeclaration {
 	ArrayList *package_names; 		// ArrayList<String>
+	char *package_name;				// String
 	ArrayList *type_identifiers; 	// ArrayList<String>
 };
 
@@ -36,6 +38,22 @@ ImportDeclaration *import_declaration_copy(ImportDeclaration *import_declaration
  * @return ArrayList* 
  */
 ArrayList *compact_import_declarations(ArrayList *import_declarations);
+
+/**
+ * @brief 
+ * 
+ * @param import_declaration 
+ * @return true 
+ */
+bool import_declaration_should_import_all(ImportDeclaration *import_declaration);
+
+/**
+ * @brief 
+ * 
+ * @param type_ident 
+ * @return true  
+ */
+bool import_declaration_contains(ImportDeclaration *import_declaration, char *type_ident);
 
 /**
  * @brief 
--- src/types/import.c
+++ src/types/import.c
@@ -11,12 +11,15 @@
 ImportDeclaration *import_declaration_new() {
     ImportDeclaration *import_declaration = (ImportDeclaration *) malloc(sizeof(ImportDeclaration));
     import_declaration->package_names = arraylist_create();
+    import_declaration->package_name = NULL;
     import_declaration->type_identifiers = arraylist_create();
     return import_declaration;
 }
 
 ImportDeclaration *import_declaration_copy(ImportDeclaration *import_declaration) {
     ImportDeclaration *new_import_declaration = import_declaration_new();
+
+    new_import_declaration->package_name = strdup(import_declaration->package_name);
 
     for (size_t i = 0; i < arraylist_size(import_declaration->package_names); i++) {
         arraylist_add(new_import_declaration->package_names, arraylist_get(import_declaration->package_names, i));
@@ -32,55 +35,129 @@ ImportDeclaration *import_declaration_copy(ImportDeclaration *import_declaration
 ArrayList *compact_import_declarations(ArrayList *import_declarations) {
     ArrayList *compacted_import_declarations = arraylist_create();
 
+    bool successful_evaluation = true;
+
     for (size_t i = 0; i < arraylist_size(import_declarations); i++) {
         ImportDeclaration *import_declaration = (ImportDeclaration *) arraylist_get(import_declarations, i);
 
-        // check if compacted_import_declarations already contains an import with the same package name
-        bool already_contains_import = false;
-        ImportDeclaration *current = NULL;
-        for (size_t j = 0; j < arraylist_size(compacted_import_declarations); j++) {
-            ImportDeclaration *compacted_import_declaration = (ImportDeclaration *) arraylist_get(compacted_import_declarations, j);
+        // check if `compact_import_declarations` already
+        // contains an import_declaration with the same package_names
 
-            // TODO: check whole package name
-            if (strcmp(arraylist_get(import_declaration->package_names, 0), arraylist_get(compacted_import_declaration->package_names, 0)) == 0) {
-                already_contains_import = true;
-                current = compacted_import_declaration;
+        bool compacted_already_contains_package = false;
+
+        ImportDeclaration *compacted_import_declaration = NULL;
+        for (size_t j = 0; j < arraylist_size(compacted_import_declarations); j++) {
+            compacted_import_declaration = (ImportDeclaration *) arraylist_get(compacted_import_declarations, j);
+
+            if (strcmp(import_declaration->package_name, compacted_import_declaration->package_name) == 0) {
+                compacted_already_contains_package = true;
                 break;
             }
         }
 
-        if (already_contains_import) {
-            // add type identifiers to existing import
-            for (size_t j = 0; j < arraylist_size(import_declaration->type_identifiers); j++) {
-                char *type_identifier = arraylist_get(import_declaration->type_identifiers, j);
+        if (compacted_already_contains_package) {
+            // we found an import_declaration with the same package_names
 
-                /// `*` is a special case, it means all types in the
-                /// package (e.g. `ìmport std.io::*;`) should be imported
-                /// 
-                /// the compiler knows that is should import all types if
-                /// `current->type_identifiers` is empty
-                if (strcmp(type_identifier, "*") == 0) {
-                    // free all type identifiers from `current`
-                    for (size_t k = 0; k < arraylist_size(current->type_identifiers); k++) {
-                        char *type_identifier = arraylist_get(current->type_identifiers, k);
-                        free(type_identifier);
-                    }
+            if (import_declaration_contains(compacted_import_declaration, "*")) {
+                printf("%s:%d:%d: " RED "error: " RESET "cannot import types after using wildcard import '*' for the same package '%s'\n",
+                        "TODO", 0, 0, import_declaration->package_name);
+                successful_evaluation = false;
+                
+                goto _exit;
+            }
 
-                    // clear arraylist (size = 0)
-                    arraylist_clear(current->type_identifiers);
-                } else {
-                    arraylist_add(current->type_identifiers, strdup(type_identifier));
+            if (import_declaration_contains(import_declaration, "*")) {
+                if (arraylist_size(compacted_import_declaration->type_identifiers) > 0) {
+                    printf("%s:%d:%d: " RED "error: " RESET "cannot use wildcard import '*' after importing types from the same package '%s'\n",
+                            "TODO", 0, 0, import_declaration->package_name);
+                    successful_evaluation = false;
+                    
+                    goto _exit;
                 }
             }
-        } else {
-            // add the import declaration to `compacted_import_declarations`
-            arraylist_add(compacted_import_declarations, import_declaration_copy(import_declaration));
+
+            // check if types are already imported
+            for (size_t j = 0; j < arraylist_size(import_declaration->type_identifiers); j++) {
+                char *type_ident = (char *) arraylist_get(import_declaration->type_identifiers, j);
+
+                if (import_declaration_contains(compacted_import_declaration, type_ident)) {
+                    printf("%s:%d:%d: " RED "error: " RESET "cannot import type '%s' twice from for the same package '%s'\n",
+                            "TODO", 0, 0, type_ident, import_declaration->package_name);
+                    successful_evaluation = false;
+
+                    goto _exit;
+                }
+
+                arraylist_add(compacted_import_declaration->type_identifiers, strdup(type_ident));
+            }
+
+            continue;
+        } // else: we did not find an import_declaration with the same package_names
+
+        // we didn't find a package_name match, so we can add the import_declaration
+        if (import_declaration_contains(import_declaration, "*")) {
+            // we found an import_declaration with the wildcard `*`
+
+            // check if import_declaration contains additional type_identifiers
+            if (arraylist_size(import_declaration->type_identifiers) > 1) {
+                printf("%s:%d:%d: " YELLOW "warning: " RESET "import_declaration contains additional type_identifiers after wildcard '*'\n",
+                       "TODO", 0, 0);
+            }
+        } // else: we found an import_declaration without the wildcard `*`
+
+        // add it to the compacted_import_declarations
+        arraylist_add(compacted_import_declarations, import_declaration_copy(import_declaration));
+    }
+
+    _exit: ;
+
+    // print all imported types
+    for (size_t i = 0; i < arraylist_size(compacted_import_declarations); i++) {
+        ImportDeclaration *import_declaration = (ImportDeclaration *) arraylist_get(compacted_import_declarations, i);
+
+        printf("%s:%d:%d: " GREEN "info: " RESET "imported types from package '%s':\n",
+                "TODO", 0, 0, import_declaration->package_name);
+
+        for (size_t j = 0; j < arraylist_size(import_declaration->type_identifiers); j++) {
+            char *type_ident = (char *) arraylist_get(import_declaration->type_identifiers, j);
+            printf("%s:%d:%d: " GREEN "info: " RESET "  - %s\n",
+                    "TODO", 0, 0, type_ident);
         }
     }
 
-    log_warning("compacted %zu to %zu imports\n", arraylist_size(import_declarations), arraylist_size(compacted_import_declarations));
+    // TODO: if successful_evaluation == false, we should free all allocated memory
+    
+    return successful_evaluation ? compacted_import_declarations : NULL;
+}
 
-    return compacted_import_declarations;
+bool import_declaration_should_import_all(ImportDeclaration *import_declaration) {
+    if (arraylist_size(import_declaration->type_identifiers) == 0) {
+        return true;
+    }
+
+    for (size_t i = 0; i < arraylist_size(import_declaration->type_identifiers); i++) {
+        char *type_identifier = arraylist_get(import_declaration->type_identifiers, i);
+        if (strcmp(type_identifier, "*") == 0) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool import_declaration_contains(ImportDeclaration *import_declaration, char *type_ident) {
+    if (import_declaration == NULL || import_declaration->type_identifiers == NULL) {
+        return false;
+    }
+    
+    for (size_t i = 0; i < arraylist_size(import_declaration->type_identifiers); i++) {
+        char *type_identifier = arraylist_get(import_declaration->type_identifiers, i);
+        if (strcmp(type_identifier, type_ident) == 0) {
+            return true;
+        }
+    }
+
+    return false;
 }
 
 void import_declaration_free(ImportDeclaration *import_declaration) {
