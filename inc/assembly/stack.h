fd3a668ba971d835f0d133fe51d7ef05964f9a9a
Luca Lewin <luca.lewin23@gmail.com>
2022-07-15 17:20:56 +0200

    Create stack.h

---
+++ inc/assembly/stack.h
@@ -0,0 +1,17 @@
+#ifndef __LUVA_X86_64_COMPILER_ASSEMBLY_STACK_H__
+#define __LUVA_X86_64_COMPILER_ASSEMBLY_STACK_H__
+
+#include <assembly/registers.h>
+
+typedef struct _StackLayout StackLayout;
+
+StackLayout* stack_layout_new();
+void stack_layout_free(StackLayout *stack_layout);
+
+void stack_push_register(StackLayout *stack_layout, Register reg, RegisterLayout *layout);
+void stack_push_variable(StackLayout *stack_layout, char *var_name);
+
+void stack_pop_register(StackLayout *stack_layout, Register reg, RegisterLayout *layout);
+void stack_pop_variable(StackLayout *stack_layout, char *var_name);
+
+#endif // __LUVA_X86_64_COMPILER_ASSEMBLY_STACK_H__
