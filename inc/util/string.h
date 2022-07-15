509021b51e4192f36945f9445ce326f2b28de06b
Luca Lewin <luca.lewin23@gmail.com>
2022-07-15 17:23:47 +0200

    remove unused functions

--- inc/util/string.h
+++ inc/util/string.h
@@ -1,13 +1,7 @@
 #ifndef __LUVA_STRING_H__
 #define __LUVA_STRING_H__
 
-char *append_string(char *dest, const char *src);
-
-char *escape(const char *str);
 char *unescape(char *src);
-
-char *to_asm_string(char *src, char *dest);
-char *to_asm_escaped_string(int arch, char *src);
 
 char unescape_char(const char *c);
 
