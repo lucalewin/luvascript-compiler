7433cb5c3e7cad74f45e81d75a6385696bca8054
Luca Lewin <luca.lewin23@gmail.com>
2022-07-15 17:22:57 +0200

    Update datatypes.h

--- inc/types/datatypes.h
+++ inc/types/datatypes.h
@@ -13,6 +13,13 @@ typedef struct _datatype {
 	unsigned long long int array_size;
 } Datatype;
 
+typedef enum _DatatypeDirective {
+	DATATYPE_DIRECTIVE_BYTE = 1,
+	DATATYPE_DIRECTIVE_WORD = 2,
+	DATATYPE_DIRECTIVE_DWORD = 4,
+	DATATYPE_DIRECTIVE_QWORD = 8
+} DatatypeDirective;
+
 int types_equal(const Datatype *a, const Datatype *b);
 int datatype_is_number(const Datatype *datatype);
 
@@ -20,5 +27,8 @@ Datatype *parse_datatype(char *type);
 Datatype *copy_datatype(const Datatype *datatype);
 
 void datatype_free(Datatype *datatype);
+
+char *to_datatype_directive(const Datatype *datatype);
+char *datatype_to_datatype_directive(const Datatype *datatype);
 
 #endif
