51e46191519022cee79859357ee97a0942f85e13
Luca Lewin <luca.lewin23@gmail.com>
2022-07-23 17:55:06 +0200

    assign values for better indexing

--- inc/generation/arch/x86-64/assembly/datatypes.h
+++ inc/generation/arch/x86-64/assembly/datatypes.h
@@ -2,21 +2,29 @@
 #define __LUVA_X86_64_COMPILER_ASSEMBLY_DATATYPES_H__
 
 typedef enum _AssemblyDataSectionTypes {
-    DATA_TYPE_DB,
-    DATA_TYPE_DW,
-    DATA_TYPE_DD,
-    DATA_TYPE_DQ,
-    DATA_TYPE_EQU
+    DATA_TYPE_EQU = 0,
+    DATA_TYPE_DB = 1,
+    DATA_TYPE_DW = 2,
+    DATA_TYPE_DD = 4,
+    DATA_TYPE_DQ = 8,
 } AssemblyDataSectionTypes;
 
 typedef enum _AssemblyBssSectionTypes {
-    BSS_TYPE_RESB,
-    BSS_TYPE_RESW,
-    BSS_TYPE_RESD,
-    BSS_TYPE_RESQ
+    BSS_TYPE_RESB = 1,
+    BSS_TYPE_RESW = 2,
+    BSS_TYPE_RESD = 4,
+    BSS_TYPE_RESQ = 8
 } AssemblyBssSectionTypes;
+
+typedef enum _DatatypeDirective {
+	DATATYPE_DIRECTIVE_BYTE = 1,
+	DATATYPE_DIRECTIVE_WORD = 2,
+	DATATYPE_DIRECTIVE_DWORD = 4,
+	DATATYPE_DIRECTIVE_QWORD = 8
+} DatatypeDirective;
 
 extern const char *_AssemblyDataSectionTypes_names[];
 extern const char *_AssemblyBssSectionTypes_names[];
+extern const char *_AssemblyDataType_directives[];
 
 #endif // __LUVA_X86_64_COMPILER_ASSEMBLY_DATATYPES_H__
